c4a5ca4eb802b56204fd766e549e1d85da77c42e
rquinteroTecSin <ricardo.quintero@tec.mx>
2022-05-28 01:45:06 +0000

    Finalizo definicion de Rectangulo...

---
+++ assignments/01-ShapeCircleRectanglePolygon/Rectangle.cpp
@@ -0,0 +1,23 @@
+#include "Rectangle.hpp"
+
+Rectangle::Rectangle() : Shape{}
+{
+    x1 = 0;
+    y1 = 0;
+}
+
+Rectangle::Rectangle(int valX, int valY, int valX1, int valY1) : Shape{valX, valY}
+{
+    x1 = valX1;
+    y1 = valY1;
+}
+
+int Rectangle::getValueX1()
+{
+    return x1;
+}
+
+int Rectangle::getValueY1()
+{
+    return y1;+
+}
